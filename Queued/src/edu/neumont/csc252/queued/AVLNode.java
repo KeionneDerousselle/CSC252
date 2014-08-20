fa98cae92d579748a1654003f047f72ebf9231e9
kderou19 <kderousselle@student.neumont.edu>
2014-08-20 14:51:46 -0600

    Queued Milestone 2 contd.

--- Queued/src/edu/neumont/csc252/queued/AVLNode.java
+++ Queued/src/edu/neumont/csc252/queued/AVLNode.java
@@ -1,5 +1,7 @@
 package edu.neumont.csc252.queued;
 
+import javax.xml.soap.Node;
+
 /**
  * Created by kderousselle on 8/16/14.
  */
@@ -50,6 +52,153 @@ public class AVLNode< T extends Comparable<T>>
         }
 
         return added;
+    }
+
+    public boolean removeNode(AVLNode<T> oldChild)
+    {
+        boolean removed = true;
+
+        if(oldChild.getValue().compareTo(this.value) == 0)
+        {
+            remove(oldChild, NodeHierarchy.ROOT);
+        }
+        else if(oldChild.getValue().compareTo(this.getValue()) < 0)
+        {
+
+            if(oldChild.equals(this.getLeftChild()))
+            {
+                remove(oldChild, NodeHierarchy.LEFT_CHILD);
+            }
+            else
+            {
+                this.getLeftChild().removeNode(oldChild);
+            }
+        }
+        else if (oldChild.getValue().compareTo(this.getValue()) > 0)
+        {
+            if(oldChild.equals(this.getRightChild()))
+            {
+                remove(oldChild, NodeHierarchy.RIGHT_CHILD);
+            }
+            else
+            {
+                this.getRightChild().removeNode(oldChild);
+            }
+        }
+
+        //calc balance factor
+        //balance
+
+
+
+
+        return removed;
+    }
+
+    private boolean remove(AVLNode<T> node, NodeHierarchy hierarchy)
+    {
+
+        if(isLeaf(node))
+        {
+            if(hierarchy.equals(NodeHierarchy.LEFT_CHILD))
+            {
+                this.setLeftChild(null);
+            }
+            else if(hierarchy.equals(NodeHierarchy.RIGHT_CHILD))
+            {
+                this.setRightChild(null);
+            }
+            else if(hierarchy.equals(NodeHierarchy.ROOT))
+            {
+                this.setValue(null);
+            }
+
+        }
+        else if(node.getLeftChild() == null && node.getRightChild() != null)
+        {
+            if(hierarchy.equals(NodeHierarchy.LEFT_CHILD))
+            {
+                this.setLeftChild(node.getRightChild());
+            }
+            else if(hierarchy.equals(NodeHierarchy.RIGHT_CHILD))
+            {
+                this.setRightChild(node.getRightChild());
+            }
+            else if(hierarchy.equals(NodeHierarchy.ROOT))
+            {
+                this.setLeftChild(this.getRightChild().getLeftChild());
+                this.setValue(this.getRightChild().getValue());
+                this.setRightChild(this.getRightChild().getRightChild());
+            }
+        }
+        else if(node.getRightChild() == null && node.getLeftChild()!= null)
+        {
+            if(hierarchy.equals(NodeHierarchy.LEFT_CHILD))
+            {
+                this.setLeftChild(node.getLeftChild());
+            }
+            else if(hierarchy.equals(NodeHierarchy.RIGHT_CHILD))
+            {
+                this.setRightChild(node.getLeftChild());
+            }
+            else if(hierarchy.equals(NodeHierarchy.ROOT))
+            {
+                this.setLeftChild(this.getLeftChild().getLeftChild());
+                this.setValue(this.getLeftChild().getValue());
+                this.setRightChild(this.getLeftChild().getRightChild());
+            }
+        }
+        else
+        {
+            AVLNode<T> successorParent = getInOrderSuccesorParent(node);
+            AVLNode<T> successor = getInOrderSuccesor(node.getRightChild());
+            node.setValue(successor.getValue());
+            successorParent.setLeftChild(successor.getRightChild());
+
+            if(hierarchy.equals(NodeHierarchy.ROOT))
+            {
+                this.setValue(node.getValue());
+                this.setRightChild(node.getRightChild());
+                this.setLeftChild(node.getLeftChild());
+            }
+        }
+
+        return true;
+
+    }
+    private AVLNode<T> getInOrderSuccesor(AVLNode<T> parent)
+    {
+        if(parent.getLeftChild() == null)
+        {
+            return parent;
+        }
+        else
+        {
+            return getInOrderSuccesor(parent.getLeftChild());
+        }
+    }
+
+    private AVLNode<T> getInOrderSuccesorParent(AVLNode<T> parent)
+    {
+        if(parent.getRightChild().getLeftChild() == null)
+        {
+            return parent;
+        }
+        else
+        {
+            parent = parent.getRightChild();
+
+            while(parent.getLeftChild().getLeftChild() != null)
+            {
+                parent = parent.getLeftChild();
+            }
+        }
+
+        return parent;
+    }
+    private boolean isLeaf(AVLNode<T> node)
+    {
+        return (node.getRightChild() == null && node.getLeftChild() == null);
     }
     public void setValue(T value) {
         this.value = value;
---
+++ Queued/src/edu/neumont/csc252/queued/NodeHierarchy.java
@@ -0,0 +1,11 @@
+package edu.neumont.csc252.queued;
+
+/**
+ * Created by kderousselle on 8/19/14.
+ */
+public enum NodeHierarchy
+{
+    LEFT_CHILD,
+    RIGHT_CHILD,
+    ROOT;
+}
